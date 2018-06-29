f0e5847b8f340912337a7f6aa209dbe199f1e530
Lancelot Bompard <lbompard@deezer.fr>
2018-06-29 10:49:02 +0200

    1er commit

---
+++ AndroidStudioProjects/Lancelot_1erApp/app/src/main/java/com/lbompard/lancelot_1erapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lbompard.lancelot_1erapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    <string name="none"></string>
+}
